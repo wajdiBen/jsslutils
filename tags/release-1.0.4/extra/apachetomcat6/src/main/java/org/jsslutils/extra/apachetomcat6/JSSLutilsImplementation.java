0c33880dc4c3ee59d4de3f7d1442c977b390cddc
Bruno.Harbulot@manchester.ac.uk <Bruno.Harbulot@manchester.ac.uk@a418cd63-9b4a-0410-9131-c17090cd975e>
2010-06-11 13:56:32 +0000

    [maven-release-plugin]  copy for tag release-1.0.4

---
+++ tags/release-1.0.4/extra/apachetomcat6/src/main/java/org/jsslutils/extra/apachetomcat6/JSSLutilsImplementation.java
@@ -0,0 +1,47 @@
+/*
+ *  This is derived from the Apache Tomcat implementation:
+ *  Copyright 1999-2010 The Apache Software Foundation.
+ *  
+ *  Derived work Copyright 2008-2010 The University of Manchester, UK.
+ *  
+ *  
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+package org.jsslutils.extra.apachetomcat6;
+
+import org.apache.tomcat.util.net.ServerSocketFactory;
+import org.apache.tomcat.util.net.jsse.*;
+
+/**
+ * This is an SSLImplementation extending the default JSSEImplementation to use
+ * <a href="http://www.jsslutils.org">jSSLutils</a>.
+ * 
+ * @author Bruno Harbulot
+ */
+public class JSSLutilsImplementation extends JSSEImplementation {
+    public JSSLutilsImplementation() throws ClassNotFoundException {
+
+    }
+
+    public String getImplementationName() {
+        return "jsslutils";
+    }
+
+    public ServerSocketFactory getServerSocketFactory() {
+        return new JSSLutilsJSSESocketFactory();
+    }
+}
